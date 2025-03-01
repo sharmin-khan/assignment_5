d83e291212674b1f0bd330617a31cc1cf8515422
sharmin-khan <sharminkhancse@gmail.com>
2025-03-01 22:41:22 +0600

    completed activity log div

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ for (const button of completeBtn) {
     event.target.setAttribute("disabled", "true");
     const taskCount = document.querySelector(".task_count");
     let taskAssigned = parseInt(taskCount.innerText);
-    if(taskAssigned > 0){
-        taskAssigned--;
-        taskCount.innerText = taskAssigned;
+    if (taskAssigned > 0) {
+      taskAssigned--;
+      taskCount.innerText = taskAssigned;
     }
-   
+
     const countTask = document.querySelector(".count_task");
     let totalCount = parseInt(countTask.innerText);
-    if(totalCount > 0){
-        totalCount++;
-        countTask.innerText = totalCount;
+    if (totalCount > 0) {
+      totalCount++;
+      countTask.innerText = totalCount;
     }
+
+    let title =
+      event.target.parentNode.parentNode.querySelector(".title").textContent;
+    const historyDiv = document.querySelector(".history_div");
+    const div = document.createElement("div");
+    div.innerHTML = `<p class="text-gray-500">You have completed the task ${title} at 12:48:15 PM</p>`;
+    historyDiv.appendChild(div);
+    document
+      .querySelector(".history_btn")
+      .addEventListener("click", function () {
+        document.querySelector(".history_div").textContent = "";
+      });
   });
 }
